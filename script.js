dc8cdf1e2ff4aa98a3b6fe5584f817044d042d04
Leondvin <krmozncr@gmail.com>
2026-07-03 16:20:32 +0300

    Add getHumanChoice()

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const playGame = () => {
   let computerScore = 0;
   let humanScore = 0;
+  
+  const getHumanChoice = () => {
+    const humanChoiceRaw = prompt("Enter your choice", "rock");
+    const humanChoice = humanChoiceRaw.toLowerCase();
+    return humanChoice;
+  };
 };
